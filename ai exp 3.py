2be7f44a975d31972fbfd19323d49f28a769451e
Vishal Sony <vishalsonyabc@gmail.com>
2020-11-25 21:00:58 +0530

    first commit

---
+++ ai exp 3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import speech_recognition as sr
+
+r=sr.Recognizer()
+
+mic = sr.Microphone(device_index=1)
+
+with mic as source:
+    r.adjust_for_ambient_noise(source,duration=1)
+    print("Please say your name:")
+    audio1=r.listen(source,timeout=40)
+    print("Please say your UID:")
+    audio2=r.listen(source,timeout=20)
+    print("Thanks!")
+    
+try:
+    print("Your name & UID is:"+r.recognize_google(audio1)+r.recognize_google(audio2));
+except:
+    print("Sorry! Unable to recognize.")
+    pass;
+
